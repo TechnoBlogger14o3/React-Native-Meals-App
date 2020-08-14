ccfe5ab074efe16bc217d74e0b9fb9a5e9f57be3
Aman Shekhar <Aman Shekhar>
2020-08-14 14:27:59 +0530

    added custom fonts

--- App.js
+++ App.js
@@ -1,6 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import * as Font from 'expo-font';
+import { AppLoading } from 'expo';
+
+
+const fetchFonts = () => {
+  Font.loadAsync({
+    'open-sans': require('./assets/fonts/OpenSans-Regular.ttf'),
+    'open-sans-bold': require('./assets/fonts/OpenSans-Bold.ttf')
+  });
+};
 
 export default function App() {
   return (
