e5143af36e6f31f9b64f15fe02f3b2cec58ad0d2
Aman Shekhar <Aman Shekhar>
2020-08-15 21:32:42 +0530

    stylling the grid with custom component

---
+++ components/CategoryGridTile.js
@@ -0,0 +1,46 @@
+import React from 'react';
+import { View, Text, TouchableOpacity, StyleSheet, Platform, TouchableNativeFeedback } from 'react-native';
+
+const CategoryGridTile = props => {
+    let TouchableCmp = TouchableOpacity;
+    if(Platform.OS === 'android' && Platform.Version >=21) {
+        TouchableCmp = TouchableNativeFeedback;
+    }
+    return ( 
+        <View style = {styles.gridItemStyle}>
+            <TouchableCmp 
+                style={{flex:1}}
+                onPress={props.onSelect}>
+                <View style = {{...styles.container, ...{ backgroundColor: props.color}}}>
+                    <Text style = {styles.title} numberOfLines={2}>{props.title}</Text>
+                </View>
+            </TouchableCmp> 
+        </View>
+    );
+}
+
+const styles = StyleSheet.create({
+    gridItemStyle: {
+        flex: 1,
+        margin: 15,
+        height: 150,
+        borderRadius: 10, 
+        overflow: 'hidden'
+    }, 
+    container: {
+        flex: 1, 
+        borderRadius: 10,
+        alignItems: 'center',
+        justifyContent: 'center',
+        elevation: 3,
+        shadowOpacity: 0.26,
+        shadowOffset: { width: 0, height: 2},
+        shadowRadius: 10
+    }, 
+    title: {
+        fontFamily: 'open-sans-bold',
+        fontSize: 18
+    }
+});
+ 
+export default CategoryGridTile;
--- screens/CategoriesScreen.js
+++ screens/CategoriesScreen.js
@@ -2,25 +2,23 @@ import React from 'react';
 import { View, Text, StyleSheet, Button, FlatList, Touchable, TouchableOpacity } from 'react-native';
 import { CATEGORIES } from '../data/dummy-data';
 import Colors from '../constants/Colors';
+import CategoryGridTile from '../components/CategoryGridTile';
 
 const CategoriesScreen = props => {
     const renderGridItem = (itemData) => {
         return (
-            <TouchableOpacity 
-                style = {styles.gridItemStyle}
-                onPress={() => {
-                props.navigation.navigate({ 
-                    routeName: 'CategoryMeals',
-                    // Passing Data through Params
-                    params: {
-                        categoryId: itemData.item.id
-                    }
-                });
-            }}>
-                <View>
-                    <Text>{itemData.item.title}</Text>
-                </View>
-            </TouchableOpacity>
+            <CategoryGridTile 
+                title={itemData.item.title} 
+                color={itemData.item.color}
+                onSelect={() => {
+                    props.navigation.navigate({ 
+                        routeName: 'CategoryMeals',
+                        params: {
+                            categoryId: itemData.item.id
+                        }
+                    });
+                }} 
+            />
         );
     };
 
@@ -42,12 +40,6 @@ const styles = StyleSheet.create({
         flex: 1,
         justifyContent: 'center',
         alignItems: 'center'
-    }, 
-    gridItemStyle: {
-        flex: 1,
-        margin: 15,
-        height: 150,
-
     }
 });
 
